744fe7ab858f98f5a640cdf64c6230b175cd9963
无尘 <sdcx-zx@outlook.com>
2025-04-19 14:54:09 +0800

    fix(internal): 修复 CORS 中间件和 MongoDB 连接日志
    - 在 CORS 中间件中，使用 strconv.Itoa 将 MaxAge 转换为字符串，确保类型正确
    - 更新 MongoDB 连接日志，使用 *clientOptions.MaxPoolSize 和 *clientOptions.MinPoolSize 以获取正确的值- 优化 fmt 输出，使用 Printf 替代 Println 以提高可读性

--- cmd/migrate/main.go
+++ cmd/migrate/main.go
@@ -11,7 +11,7 @@ func main() {
 	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
 	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
 	s := "gopher"
-	fmt.Println("Hello and welcome, %s!", s)
+	fmt.Printf("Hello and welcome, %s!\n", s)
 
 	for i := 1; i <= 5; i++ {
 		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
